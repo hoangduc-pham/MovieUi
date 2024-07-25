6dbb2951e8eb6bbb0b391440a2a1a9c1d9de80f6
Phạm Đức Hoàng <hoangluckyexoo@gmail.com>
2024-07-25 16:18:51 +0700

    update dispacherio in TypeViewmodel

--- app/src/main/java/com/hoangpd15/smartmovie/ui/homeScreen/typeMovieScreen/TypeViewModel.kt
+++ app/src/main/java/com/hoangpd15/smartmovie/ui/homeScreen/typeMovieScreen/TypeViewModel.kt
@@ -3,27 +3,37 @@ package com.hoangpd15.smartmovie.ui.homeScreen.typeMovieScreen
 import com.hoangpd15.smartmovie.model.Movie
 import com.hoangpd15.smartmovie.model.dataRemote.RetrofitInstance
 import com.hoangpd15.smartmovie.base.BaseViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class NowPlayingViewModel : BaseViewModel() {
     override suspend fun fetchMoviesFromApi(page: Int): List<Movie> {
-        return RetrofitInstance.apiMovieNowPlaying.getNowPlayingMovies(page).results
+        return withContext(Dispatchers.IO) {
+            RetrofitInstance.apiMovieNowPlaying.getNowPlayingMovies(page).results
+        }
     }
 }
 
 class PopularViewModel : BaseViewModel() {
     override suspend fun fetchMoviesFromApi(page: Int): List<Movie> {
-        return RetrofitInstance.apiMoviePopular.getPopularMovies(page).results
+        return withContext(Dispatchers.IO) {
+            RetrofitInstance.apiMoviePopular.getPopularMovies(page).results
+        }
     }
 }
 
 class TopRatedViewModel : BaseViewModel() {
     override suspend fun fetchMoviesFromApi(page: Int): List<Movie> {
-        return RetrofitInstance.apiMovieTopRate.getTopRateMovies(page).results
+        return withContext(Dispatchers.IO) {
+            RetrofitInstance.apiMovieTopRate.getTopRateMovies(page).results
+        }
     }
 }
 
 class UpComingViewModel : BaseViewModel() {
     override suspend fun fetchMoviesFromApi(page: Int): List<Movie> {
-        return RetrofitInstance.apiMovieUpComing.getUpComingMovies(page).results
+        return withContext(Dispatchers.IO) {
+            RetrofitInstance.apiMovieUpComing.getUpComingMovies(page).results
+        }
     }
 }
